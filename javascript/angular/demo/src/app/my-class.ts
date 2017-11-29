75104c0b1208bba8206857c614f8929349003321
makiroggers <makiroggers@gmail.com>
2017-11-29 11:45:34 -0800

    Class component code-along demo

--- javascript/angular/demo/src/app/my-class.ts
+++ javascript/angular/demo/src/app/my-class.ts
@@ -1,2 +1,83 @@
+// import * as AppModule from 'AppModule'; // import all from x
+// import { SpecificLibraryObject } from 'assets'; // import 1 from x
+// import { objectA, objectB } from 'filepath/LibaryName'; // import 2 from x
+// import * as rxjs from 'rxjs';
+// import observable from 'rxjs/observable';
+
 export class MyClass {
+  // TSLint syntax for variables
+  myStringMC: 'This is a string';
+  myNumberMC: 7;
+  myBooleanMC: true;
+  myNumArrayMC1: number[];
+  myNumArrayMC2: Array<number>;
+  anythingMC: any = 7;
 }
+
+// Platform syntax for variables
+// var myString = 'This is a string';
+// var myNumber = 7;
+// var myBoolean = true;
+// var myNumArray = number[];
+// var myNumArray = Array<number>
+// var anything: any = 7;
+
+function testing() {
+  let x = 10;
+  console.log(x);
+  x = 20;
+}
+
+if (true) {
+  const x = 10;
+  console.log(x);
+}
+
+const myobj = {
+  a: 1,
+  b: 2
+};
+
+myobj.a = 5;
+
+// Never type never reaches the end of a function
+function errorHandler(message: string): never {
+  throw new Error(message);
+}
+
+// Void returns nothing
+function printValue(val: string): void {
+  console.log(val);
+  return;
+}
+
+// Class construction using TS
+class SLNode {
+  val: number;
+
+  constructor(valueP: number) {
+    this.val = valueP;
+  }
+  doSomethingFun() {
+    console.log('This is fun!');
+  }
+}
+
+const firstSLNode = new SLNode(1);
+
+// export { SampleClassA as NewClassName }; // Export a class with a different name.
+
+// Alternative
+// class SampleClassA {
+//   sampleMethod(aStr: string): number {
+//     return aStr.length();
+//   }
+// }
+
+// class SampleClassB {
+//   sampleMethod(aStr: string): number {
+//     return aStr.length();
+//   }
+// }
+
+// export { SampleClassA, SampleClassB }; // Export 2 Classes.
---
+++ javascript/angular/demo/src/app/user-component/user-component.component.ts
@@ -0,0 +1,22 @@
+import { any } from 'codelyzer/util/function';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
+
+@Component({
+  selector: 'app-user-component',
+  templateUrl: './user-component.component.html',
+  styleUrls: ['./user-component.component.css']
+})
+export class UserComponentComponent implements OnInit {
+  my_name: string;
+  other_names: Array<User>;
+  constructor() { }
+
+  ngOnInit() {
+    this.my_name = 'Maki';
+    this.other_names = [{first_name: 'Lizzie', last_name: 'Guiterrez', email: 'lizzie@lizzie.com', important: true},
+                        {first_name: 'Mary', last_name: 'MaryMary', email: 'mary@mary.com', important: true},
+                        {first_name: 'Jennifer', last_name: 'Feng', email: 'jen@jen.com', important: true}];
+  }
+
+}

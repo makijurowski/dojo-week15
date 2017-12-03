0b083d68683bef6bd5c6f9192b9153e523bdb8c8
makiroggers <makiroggers@gmail.com>
2017-12-03 12:19:33 -0800

    Added KendoUI support

--- csharp/aspnet_starter/client/components/Layout/Navigation.js
+++ csharp/aspnet_starter/client/components/Layout/Navigation.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import * as kendo from '@progress/kendo-ui';
+/* Alternative approaches to load Kendo UI will be:
+import '@progress/kendo-ui';
+require("@progress/kendo-ui/js/kendo.all.js"); */
+import '@progress/kendo-theme-default/dist/all.css';
+import { Calendar } from '@progress/kendo-dateinputs-react-wrapper';
 import Link from '../Link';
 
 class Navigation extends React.Component {
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     dateTime: new Date()
+  //   };
+
+  //   this.onchange = this.onchange.bind(this);
+  // }
+
+  // onchange(e) {
+  //   this.setState({
+  //     dateTime: e.sender.value()
+  //   });
+  // }
 
   componentDidMount() {
     window.componentHandler.upgradeElement(this.root);
@@ -17,6 +38,7 @@ class Navigation extends React.Component {
         <Link className="mdl-navigation__link" to="/">Home</Link>
         <Link className="mdl-navigation__link" to="/about">About</Link>
         <Link className="mdl-navigation__link" to="/not-found">Not Found</Link>
+        {/* <Calendar value={this.state.dateTime} change={this.onchange}/> */}
       </nav>
     );
   }

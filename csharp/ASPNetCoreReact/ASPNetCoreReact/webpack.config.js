ab366034a230b6405ec5f2717ff822ea484db2df
makiroggers <makiroggers@gmail.com>
2017-12-03 03:15:38 -0800

    Added project files

--- csharp/ASPNetCoreReact/ASPNetCoreReact/webpack.config.js
+++ csharp/ASPNetCoreReact/ASPNetCoreReact/webpack.config.js
@@ -1,7 +1,14 @@
 ﻿const path = require('path');
 const webpack = require('webpack');
+const materialize = require("materialize-loader");
 
 module.exports = {
+  module: {
+    loaders: [
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
+    ]
+  },
   entry: './wwwroot/source/app.js',
   output: {
             path: path.resolve(__dirname, 'wwwroot/dist'),

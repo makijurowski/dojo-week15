663a6ca029998364e35832d5e53b3ba5c3d34d58
makiroggers <makiroggers@gmail.com>
2017-12-03 16:28:11 -0800

    Fixed random errors

--- csharp/aspnet_starter/client/views/home/Home.js
+++ csharp/aspnet_starter/client/views/home/Home.js
@@ -7,7 +7,7 @@ const link = 'https://makiroggers.com';
 class Home extends React.Component {
 
   static propTypes = {
-    articles: PropTypes.array.isRequired,
+    articles: PropTypes.arrayOf.isRequired,
   };
 
   componentDidMount() {
@@ -19,14 +19,26 @@ class Home extends React.Component {
       <Layout>
         <h1 className="mdl-typography--title">Welcome to {title}!</h1>
         <p className="mdl-typography--body-1">
-          For more information visit <a href={link}>{link}</a>
+            For more information visit <a href={link}>{link}</a>
         </p>
-        <h4 className="mdl-typography--title">Articles</h4>
+        <hr />
+        <h4 className="mdl-typography--title">Included Technologies</h4>
         <ul>
-          {this.props.articles.map((article, i) =>
-            <li key={i}><a href={article.url}>{article.title}</a> by {article.author}</li>
-          )}
+          {(this.props.articles.map((article, i) =>
+            (<li key={i}>
+              <a href={article.url}>{article.title}</a> by {article.author}
+              <ul>
+                {article.includes.map((product, n) =>
+                  (<li key={n}>
+                    <a href={product.url}>{product.title}</a> ({product.author})
+                  </li>),
+                )}
+              </ul>
+            </li>),
+          ))}
         </ul>
+        <p />
+        <hr />
       </Layout>
     );
   }
